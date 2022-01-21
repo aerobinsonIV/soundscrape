0c47fdf616f7a0e131a8197dee3a564eaf83d31f
Iv <aerobinsoniv@gmail.com>
2022-01-20 22:57:07 -0500

    lyric getter script takes in artist and title

---
+++ lyric_getter.py
@@ -0,0 +1,11 @@
+import sys
+
+if(len(sys.argv) < 2):
+    print("Please specify artist artist and title.")
+    print("E.g: python3 lyric_getter.py \"Jousboxx, Fyrebreak, Joelle J\" \"Beyond\"")
+    exit()
+
+artist = sys.argv[1]
+title = sys.argv[2]
+
+print(f"Artist: {artist}, Title: {title}")
