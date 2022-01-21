ea4df2706a8bd0809dd6e92303176d4b736c56cc
Iv <aerobinsoniv@gmail.com>
2022-01-21 00:20:33 -0500

    We can extract the link to the song's lyrics page from the search results

--- lyric_getter.py
+++ lyric_getter.py
@@ -1,10 +1,26 @@
 import sys
 import requests
+from bs4 import BeautifulSoup
 
 def get_lyrics_azlyrics(artist, title):
-    x = requests.get(f'https://search.azlyrics.com/search.php?q={artist}+{title}')
+    search_results_page_response = requests.get(f'https://search.azlyrics.com/search.php?q={artist}+{title}')
 
-    print(x.text)
+    search_html = search_results_page_response.text
+
+    # Get the HTML of the search results page into BeautifulSoup
+    soup = BeautifulSoup(str(search_html), 'html.parser')
+
+    # Find anchor tags
+    anchors = soup.find_all('a')
+    
+    lyrics_page_url = ""
+
+    for anchor in anchors:
+        # Valid links to lyrics pages have format like '1. "Breathe" - Mako'. All other a tags don't have periods.
+        if("." in anchor.text):
+            lyrics_page_url = anchor['href']
+
+    print(lyrics_page_url)
 
 if(len(sys.argv) < 2):
     print("Please specify artist artist and title.")
