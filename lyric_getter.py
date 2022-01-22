fed8a5ae46ff8a6747e176d1d471566315062be9
Iv <aerobinsoniv@gmail.com>
2022-01-22 01:00:51 -0500

    AZlyrics method works for Mako - Breathe

--- lyric_getter.py
+++ lyric_getter.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
+from bs4 import Comment
 
 def get_lyrics_azlyrics(artist, title):
     search_results_page_response = requests.get(f'https://search.azlyrics.com/search.php?q={artist}+{title}')
@@ -8,10 +9,10 @@ def get_lyrics_azlyrics(artist, title):
     search_html = search_results_page_response.text
 
     # Get the HTML of the search results page into BeautifulSoup
-    soup = BeautifulSoup(str(search_html), 'html.parser')
+    search_soup = BeautifulSoup(str(search_html), 'html.parser')
 
     # Find anchor tags
-    anchors = soup.find_all('a')
+    anchors = search_soup.find_all('a')
     
     lyrics_page_url = ""
 
@@ -20,7 +21,40 @@ def get_lyrics_azlyrics(artist, title):
         if("." in anchor.text):
             lyrics_page_url = anchor['href']
 
-    print(lyrics_page_url)
+    # Get lyrics page
+    lyrics_page_response = requests.get(lyrics_page_url)
+
+    lyrics_html = lyrics_page_response.text
+
+    lyrics_soup = BeautifulSoup(str(lyrics_html), 'html.parser')
+
+    # The lyrics section is a div.
+    divs = lyrics_soup.find_all('div')
+
+    # The div containing the lyric starts with this comment
+    licensing_comment_text = ' Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. '
+    
+    most_breaks = 0
+    most_breaks_index = 0
+    for i, div in enumerate(divs):
+        
+        # Get all comments in this div
+        comments = div.find_all(string=lambda text: isinstance(text, Comment))
+        
+        # If the div has a comment, it might be our target div
+        if(len(comments) > 0):
+            # Check if the first comment is the licensing warning
+            if(comments[0] == licensing_comment_text):
+                
+                divs_in_lyric_div_candidate = div.find_all('div')
+
+                # The lyrics div has no divs nested inside it
+                if(len(divs_in_lyric_div_candidate) == 0):
+                    # If we've passed all these tests, we definetly have the lyrics
+                    return div.text.strip()
+
+    return ""
+
 
 if(len(sys.argv) < 2):
     print("Please specify artist artist and title.")
@@ -32,4 +66,4 @@ title = sys.argv[2]
 
 print(f"Artist: {artist}, Title: {title}")
 
-get_lyrics_azlyrics(artist, title)
+print(get_lyrics_azlyrics(artist, title))
