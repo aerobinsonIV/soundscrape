0bf4963e8796f7f4877941901550cf0b6b921f7a
Iv <aerobinsoniv@gmail.com>
2022-05-14 23:29:04 -0400

    AZlyrics has better matching of links to title/artist

--- lyric_getter.py
+++ lyric_getter.py
@@ -3,31 +3,67 @@ import requests
 from bs4 import BeautifulSoup
 from bs4 import Comment
 
+from fuzzywuzzy import fuzz # Fuzzy string matching library
+
 from soup_url import soup_url
 
-def get_lyrics_azlyrics(artist, title):
+def match_confidence(real_title, real_artist, test_title, test_artist):
+    title_ratio = fuzz.ratio(real_title, test_title)
+    artist_ratio = fuzz.ratio(real_artist, test_artist)
 
-    search_soup = soup_url(f'https://search.azlyrics.com/search.php?q={artist}+{title}')
+    # Exclude results that are very certainly wrong
+    if title_ratio < 50:
+        title_ratio = 0
+
+    if artist_ratio < 50:
+        artist_ratio = 0
+
+    return artist_ratio + title_ratio
+
+def get_lyrics_azlyrics(a_artist, a_title):
+
+    search_soup = soup_url(f'https://search.azlyrics.com/search.php?q={a_artist}+{a_title}')
+    
+    # Find bold tags
+    bolds = search_soup.find_all('b')
+
+    for bold in bolds:
+        # Song results pane is labelled by a b tag with this text
+        if bold.text == "Song results:":
+            song_results_pane = bold.parent.parent
+            break
 
     # Find anchor tags
-    anchors = search_soup.find_all('a')
-    
-    lyrics_page_url = ""
+    anchors = song_results_pane.find_all('a')
 
     found_result = False
+    best_confidence = 0
 
     for anchor in anchors:
         # Valid links to lyrics pages have format like '1. "Breathe" - Mako'. All other a tags don't have periods.
         if("." in anchor.text):
-            lyrics_page_url = anchor['href']
+
+            # just be happy it's not regex
+            a_title = anchor.text[4:anchor.text[4:].find("\"") + 4]
+            a_artist = anchor.text[anchor.text[1:].find("-") + 3:]
+
+            confidence = match_confidence(title, artist, a_title, a_artist)
+            if confidence > best_confidence:
+                best_url = anchor['href']
+                best_confidence = confidence
+            
             # We found at least one result
             found_result = True
 
     if not found_result:
         return None
 
-    # Get lyrics page
-    lyrics_soup = soup_url(lyrics_page_url)
+    if best_confidence == 0:
+        # None of the results were even close
+        return None
+
+    # Get lyrics page from link that best matched input title and artist
+    lyrics_soup = soup_url(best_url)
 
     # The lyrics section is a div.
     divs = lyrics_soup.find_all('div')
@@ -35,8 +71,6 @@ def get_lyrics_azlyrics(artist, title):
     # The div containing the lyric starts with this comment
     licensing_comment_text = ' Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. '
     
-    most_breaks = 0
-    most_breaks_index = 0
     for i, div in enumerate(divs):
         
         # Get all comments in this div
