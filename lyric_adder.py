b1bc3cc05e030966dc23fe5928dd338fefea3ab5
Iv <aerobinsoniv@gmail.com>
2022-06-27 22:27:49 -0400

    ADDING LYRICS WORKS WOOO

--- lyric_adder.py
+++ lyric_adder.py
@@ -1,21 +1,18 @@
+import os
+import sys
+sys.path.insert(0, os.path.join(os.getcwd(), "stagger"))
 import stagger
 from stagger.id3 import *
-import os
 
 # Will assume file with name song_file is in the temp/ folder
 def add_lyrics(song_file, lyrics):
     path = os.path.join(os.getcwd(), "temp")
 
     # Open tag on song file
-    # tag = stagger.read_tag(os.path.join(path, song_file))
+    tag = stagger.read_tag(os.path.join(path, song_file))
     
-    # Create new tag
-    tag = stagger.Tag23()
-    
-    # UnSynchronized Lyrics/Text
-    tag[USLT] = lyrics
-    # tag[TPE1] = lyrics
+    tag['USLT'] = "eng|" + lyrics
     tag.write(os.path.join(path, song_file))
 
 if __name__ == "__main__":
-    add_lyrics("trippp3.mp3", "poggers")
+    add_lyrics("trippp3.mp3", "poggers\nweufbwieufbwiuefbwiefbu\nweofwubireubergergergeg")
