18e54e1c5a5257b62e724787eb4fa0e31930f693
Iv <aerobinsoniv@gmail.com>
2022-07-01 00:12:03 -0400

    Attempt full flow of adding lyrics to an existing song file, stagger broke again

--- lyric_adder.py
+++ lyric_adder.py
@@ -1,30 +1,31 @@
 import os
 import sys
+
+from lyric_getter import get_lyrics_azlyrics, get_lyrics_genius
 sys.path.insert(0, os.path.join(os.getcwd(), "stagger"))
 import stagger
 from stagger.id3 import *
+from metadata_scanner import scan_file
 
-# Will assume file with name song_file is in the temp/ folder
 def add_lyrics(song_file, lyrics):
-    path = os.path.join(os.getcwd(), "temp")
 
     # Open tag on song file
-    tag = stagger.read_tag(os.path.join(path, song_file))
+    tag = stagger.read_tag(song_file)
     
     tag['USLT'] = "eng|" + lyrics
-    tag.write(os.path.join(path, song_file))
+    tag.write(song_file)
 
 def notepad(lyrics):
 
     lyric_file_path = os.path.join(os.path.join(os.getcwd(), "temp"), "lyrics.txt")
 
-    with open(lyric_file_path, "w") as f:
+    with open(lyric_file_path, "w", encoding='utf8') as f:
         f.write(lyrics)
 
     # move along hackers, nothing to see here
     os.system("notepad " + lyric_file_path)
 
-    with open(lyric_file_path, "r") as f:
+    with open(lyric_file_path, "r", encoding='utf8') as f:
         edited_lyrics = f.read()
 
     os.remove(lyric_file_path)
@@ -32,4 +33,23 @@ def notepad(lyrics):
     return(edited_lyrics)
 
 if __name__ == "__main__":
-    print(notepad("pee pee poo poo\nweee weee wooo q"))
+    # Check args
+    # Open file/scan metadata
+    # Get lyrics for that artist/title
+    # Notepad the lyrics
+    # Write lyrics to file
+
+    if(len(sys.argv) < 1):
+        print("Please specify a file.")
+        exit()
+
+    filename = sys.argv[1]
+    scanned_title, scanned_artist = scan_file(filename)
+    
+    lyrics = get_lyrics_genius(scanned_artist, scanned_title)
+
+    edited_lyrics = notepad(lyrics)
+
+    print(f"Edited lyrics: {edited_lyrics}")
+
+    add_lyrics(filename, edited_lyrics)
