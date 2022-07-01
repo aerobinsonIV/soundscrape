624c18ade7f5c1925d1f3004f2458e68a05bcf3a
Iv <aerobinsoniv@gmail.com>
2022-06-30 23:58:57 -0400

    Metadata scanner works

--- metadata_scanner.py
+++ metadata_scanner.py
@@ -1,7 +1,17 @@
 import sys
+import os
+sys.path.insert(0, os.path.join(os.getcwd(), "stagger"))
+import stagger
+from stagger.id3 import *
 
 def scan_file(filename):
-    return ("Sandstorm", "Darude")
+    # Open tag on song file
+    tag = stagger.read_tag(filename)
+
+    title = tag['TIT2'].text[0]
+    artist = tag['TPE1'].text[0]
+
+    return (title, artist)
 
 if __name__ == "__main__":
     if(len(sys.argv) < 1):
