dce62658473dbae547d74a33bd439f00df8c258c
Iv <aerobinsoniv@gmail.com>
2022-10-15 21:39:27 -0400

    Super basic youtube-dl wrapper functionality works

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 def youtube_dl_wrapper(link: str, transcode_to_mp3: bool = False, cover_artwork: bool = False, music: bool = False):
+    
     args = "--extract-audio "
     if transcode_to_mp3:
         args += "--audio-format mp3 --audio-quality 128k "
@@ -14,14 +15,26 @@ def youtube_dl_wrapper(link: str, transcode_to_mp3: bool = False, cover_artwork:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("target", help="YouTube link or path of a file containing a list of YouTube links")
     parser.add_argument("-t", "--transcode-to-mp3", help="Transcode the downloaded opus file to mp3, lower quality but better compatibility", action='store_true')
     parser.add_argument("-c", "--cover-artwork", help="Embed video thumbnail as cover artwork", action='store_true')
     parser.add_argument("-m", "--music", help="Treat this as a YouTube music link (rather than e.g. a music video) and get the title, artist, and year from the webpage.", action='store_true')
-    link_sources = parser.add_mutually_exclusive_group()
-    link_sources.add_argument("-l", "--link-list", help="Pass in a text file containing a list of links for batch processing (one on each line).")
-    link_sources.add_argument("-s", "--single-link", help="Pass a single URL to a YouTube song or video")
     
     args = parser.parse_args()
 
-    if args.single_link != None:
-        youtube_dl_wrapper(args.single_link, args.transcode_to_mp3, args.cover_artwork, args.music)
+    print(args.target)
+
+    # Download songs to the temp folder
+    os.chdir("./temp")
+
+    if args.target[:7] == "http://" or args.target[:8] == "https://" or args.target[:4] == "www.":
+        # URL
+        youtube_dl_wrapper(args.target, args.transcode_to_mp3, args.cover_artwork, args.music)
+    else:
+        # Path to file containing list of links
+        with open(args.target, 'r') as f:
+            lines = f.readlines()
+
+        for line in lines:
+                print(f"Downloading {line}", end="")
+                youtube_dl_wrapper(line, args.transcode_to_mp3, args.cover_artwork, args.music)
